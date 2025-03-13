fd721e564778d049c8117bdec67d3f26efe2c2c5
Usman Zahid <developerusman@yahoo.com>
2025-03-13 20:20:16 +0500

    Added the favicon

--- resources/views/layouts/base_layout.blade.php
+++ resources/views/layouts/base_layout.blade.php
@@ -20,6 +20,17 @@
     <link href="{{asset('assets/css/template_css/owl.theme.default.min.css')}}" rel="stylesheet">
     <link href="{{asset('assets/css/template_css/tooplate-gotto-job.css')}}" rel="stylesheet">
 
+
+    {{-- Favicon --}}
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/images/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('assets/images/favicon/site.webmanifest') }}">
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon/favicon.ico') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('assets/images/favicon/android-chrome-192x192.png') }}">
+    <link rel="icon" type="image/png" sizes="512x512" href="{{ asset('assets/images/favicon/android-chrome-512x512.png') }}">
+
+
     {{--  Custom styles pushed  --}}
     @stack('styles')
 </head>
